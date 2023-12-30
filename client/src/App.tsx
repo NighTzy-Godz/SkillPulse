296c547faecb90b1d7b271d51f13ae7ffde2b579
Aser <ajhubero16@gmail.com>
2023-12-30 11:16:55 +0800

    Client Side - Configuration of Tailwind, Flowbite, Fonts aand folder structure

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "flowbite-react";
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <p>Im the app</p>
+      <Button>Hi</Button>
+    </div>
+  );
+}
+
+export default App;
