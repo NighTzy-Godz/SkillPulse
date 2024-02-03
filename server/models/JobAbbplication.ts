aeefb3bc6ca7a965b409f9e8a6ef8ecfddc80e94
Aser <ajhubero16@gmail.com>
2024-02-03 18:30:34 +0800

    Server Side - Added the Job Application Schema

---
+++ server/models/JobAbbplication.ts
@@ -0,0 +1,49 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+const DB_URL = process.env.DB_URL as string;
+
+mongoose
+  .connect(DB_URL)
+  .then(() => console.log("Connected to the Database - Job Application"))
+  .catch((err) => console.log(`Error on Job Application, ${err}`));
+
+enum ApplicationStatus {
+  PENDING = "Pending",
+  REVIEWING = "Reviewing",
+  REJECTED = "Rejected",
+  ACCEPTED = "Accepted",
+}
+
+interface IJobApplication extends Document {
+  jobId: mongoose.Schema.Types.ObjectId;
+  userId: mongoose.Schema.Types.ObjectId;
+  status: ApplicationStatus;
+  resume: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const jobApplicationSchema: Schema<IJobApplication> = new mongoose.Schema({
+  jobId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Job",
+  },
+
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+
+  status: {
+    type: String,
+    enum: Object.values(ApplicationStatus),
+    default: ApplicationStatus.PENDING,
+  },
+});
+
+const JobApplication = mongoose.model<IJobApplication>(
+  "JobApplication",
+  jobApplicationSchema
+);
+
+export default JobApplication;
