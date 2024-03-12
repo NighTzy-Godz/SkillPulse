b034f278baa095c8868b3533d0262bdf65508ee2
Aser <ajhubero16@gmail.com>
2024-03-12 10:41:47 +0800

    Client Side - Added the Footer

---
+++ client/src/components/ui/Footer.tsx
@@ -0,0 +1,14 @@
+function FooterNav() {
+  return (
+    <div className="py-5 boxShadow2">
+      <div className="container mx-auto">
+        <div className="flex justify-between">
+          <p className="text-zinc-500 font-semibold">&copy; SkillPulse, 2024</p>
+          <p className="text-zinc-500">Developed By: Aser James Hubero</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FooterNav;
--- client/src/pages/static/HomeLayout.tsx
+++ client/src/pages/static/HomeLayout.tsx
@@ -2,30 +2,18 @@ import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import MainNav from "../../components/ui/MainNav";
 import MainLoader from "../../components/ui/MainLoader";
+import FooterNav from "../../components/ui/Footer";
 
 interface HomeLayoutProps {
   token: null | string;
 }
 
 function HomeLayout({ token }: HomeLayoutProps) {
-  // const [ready, setReady] = useState(false);
-  // const [timer, setTimer] = useState(0);
-  // useEffect(() => {
-  //   if (ready) return;
-  //   if (timer === 6) return setReady(true);
-  //   const timerId = setTimeout(() => {
-  //     setTimer(timer + 1);
-  //   }, 1000);
-
-  //   return () => clearTimeout(timerId);
-  // }, [ready, timer]);
-  // if (!ready) {
-  //   return <MainLoader />;
-  // }
   return (
     <React.Fragment>
       <MainNav token={token} />
       <Outlet />
+      <FooterNav />
     </React.Fragment>
   );
 }
